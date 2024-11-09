33129eb8c6aff5600e5378c7a1896432e27fbf69
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-11-09 09:24:34 +0700

    fix setup router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
+import Header from "./components/layouts/Header";
+import Menu from "./components/layouts/Menu";
 import LoginPage from "./components/pages/LoginPage";
+import RegisterPage from "./components/pages/RegisterPage";
+import { Link, Navigate, Route, RouteProps, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <>
-    <div>main</div>
-      {/* <LoginPage /> */}
+      <Header />
+      <Menu />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
     </>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,34 +1,9 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import App from "./App";
-import LoginPage from "./components/pages/LoginPage";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "*",
-    element: <LoginPage />,
-  },
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  }
-]);
-
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
-} else {
-  console.error("Root element not found");
-}
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
