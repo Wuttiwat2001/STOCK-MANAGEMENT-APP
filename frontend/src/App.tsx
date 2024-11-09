457721b6195edc701ce73090c5eaef37320cc258
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-11-09 09:39:09 +0700

    appbar and drawer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,7 @@
+import { styled, useTheme, Theme, CSSObject } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
 import Header from "./components/layouts/Header";
 import Menu from "./components/layouts/Menu";
 import LoginPage from "./components/pages/LoginPage";
@@ -5,16 +9,31 @@ import RegisterPage from "./components/pages/RegisterPage";
 import { Link, Navigate, Route, RouteProps, Routes } from "react-router-dom";
 
 export default function App() {
+  const DrawerHeader = styled("div")(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  }));
+
   return (
     <>
-      <Header />
-      <Menu />
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <Header />
+        <Menu />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <DrawerHeader />
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+        </Box>
+      </Box>
     </>
   );
 }
